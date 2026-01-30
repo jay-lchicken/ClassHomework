8e236f68ba9553a56bf049246c0a148cd4924c53
Lai Hong Yu <joyjay606@gmail.com>
2026-01-30 10:29:16 +0800

    fixed bug

--- src/app/api/addHomework/route.js
+++ src/app/api/addHomework/route.js
@@ -89,8 +89,16 @@ export async function POST(req) {
         `INSERT INTO homework (homework_text, due_date, email, name, subject, link)
          VALUES ($1, $2, $3, $4, $5, $6)
            RETURNING *`,
-        [homework, due_date, email, displayName, formattedSubject, (code ? `https://link.s304.xyz/${code}`: null)]
+        [
+          homework,
+          due_date,
+          email,
+          displayName,
+          formattedSubject,
+          code != null ? `https://link.s304.xyz/${code}` : null
+        ]
     );
+
     return new Response(JSON.stringify(insertResult.rows[0]), {
       headers: { "Content-Type": "application/json" },
     });
